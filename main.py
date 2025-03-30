b9234d4e33a86105ef81166cce37e85cc4e0f64c
RhinoCoder <abbilginn@hotmail.com>
2025-03-30 23:47:50 +0300

    Tokenization & many debug statements are added for better understanding.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tiktoken.load import load_tiktoken_bpe
 import torch
 import json
 import matplotlib.pyplot as plt
-
+from torch.utils.backcompat import keepdim_warning
 
 tokenizerPath = "/Users/teatone/.llama/checkpoints/Llama3.2-1B/tokenizer.model"
 modelPath = "/Users/teatone/.llama/checkpoints/Llama3.2-1B"
@@ -50,4 +50,88 @@ with open(modelPath+"/params.json","r") as f:
 dim = config["dim"]
 numberOfTransformerLayers = config["n_layers"]
 numberOfAttentionHeads = config["n_heads"]
+nKvHeads = config["n_kv_heads"]
+vocabSize = config["vocab_size"]
+multipleOf = config["multiple_of"]
+ffnDimMultiplier = config["ffn_dim_multiplier"]
+normEps = config["norm_eps"]
+ropeTheta = torch.tensor(config["rope_theta"])
 
+#6
+#Converting text to tokens.
+prompt = "The answer to the ultimate question of life, the universe, and everything is "
+tokens = [128000] + tokenizer.encode(prompt)
+print(tokens)
+tokens = torch.tensor(tokens)
+promptSplitAsTokens = [tokenizer.decode([token.item()]) for token in tokens]
+print(promptSplitAsTokens)
+
+
+#7
+embeddingLayer = torch.nn.Embedding(vocabSize,dim)
+embeddingLayer.weight.data.copy_(model["tok_embeddings.weight"])
+tokenEmbeddingsUnnormalized = embeddingLayer(tokens).to(torch.bfloat16)
+print(tokenEmbeddingsUnnormalized.shape)
+
+#8 RMS
+def RmsNorm(tensor,normWeights):
+    return (tensor * torch.rsqrt(tensor.pow(2).mean(-1,keepdim=True)))
+
+#9 First layer of transformer
+tokenEmbeddings = RmsNorm(tokenEmbeddingsUnnormalized,model["layers.0.attention_norm.weight"])
+print(tokenEmbeddings.shape)
+
+
+#10
+print(
+    model["layers.0.attention.wq.weight"].shape,
+    model["layers.0.attention.wk.weight"].shape,
+    model["layers.0.attention.wv.weight"].shape,
+    model["layers.0.attention.wo.weight"].shape
+)
+
+#11
+qLayer0 = model["layers.0.attention.wq.weight"]
+headDim = qLayer0.shape[0] // numberOfAttentionHeads
+qLayer0 = qLayer0.view(numberOfAttentionHeads,headDim,dim)
+print(qLayer0.shape)
+
+#12
+qLayer0Head0 = qLayer0[0]
+print(qLayer0Head0.shape)
+
+#13
+tokenEmbeddings = tokenEmbeddings.to(device)
+qLayer0Head0 = qLayer0Head0.to(device)
+qPerToken = torch.matmul(tokenEmbeddings,qLayer0Head0.T).to(device)
+torch.mps.synchronize()
+print("Q Per Token Shape:\t" +str(qPerToken.shape))
+
+#14 Position Encoding
+qPerTokenSplitIntoPairs = qPerToken.float().view(qPerToken.shape[0],-1,2)
+print(qPerTokenSplitIntoPairs.shape)
+
+#15
+zeroToOneSplitInto64Parts = torch.tensor(range(64)) / 64
+print(zeroToOneSplitInto64Parts)
+frequencies = 1.0 / (ropeTheta ** zeroToOneSplitInto64Parts)
+print("Frequencies: " + str(frequencies))
+
+frequenciesForEachToken = torch.outer(torch.arange(17),frequencies)
+frequenciesCis = torch.polar(torch.ones_like(frequenciesForEachToken),frequenciesForEachToken)
+print(frequenciesCis.shape)
+
+value = frequenciesCis[3]
+plt.figure()
+for i,element in enumerate(value[:17]):
+    plt.plot([0,element.real],[0,element.imag],color='blue',linewidth=1,label=f"Index: {i}")
+    plt.annotate(f"{i}",xy = (element.real,element.imag),color = 'red' )
+
+plt.xlabel('Real')
+plt.ylabel('Imaginary')
+plt.title('Plot of the one row of frequenciesCis')
+plt.show()
+
+#16
+qPerTokenAsComplexNumbers = torch.view_as_complex(qPerTokenSplitIntoPairs)
+print(qPerTokenAsComplexNumbers.shape)
